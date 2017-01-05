08f7b3b76c36b35a89ebf6613aec80f0a5401aa3
Brian Foster <fosterbd@iupui.edu>
2017-01-04 23:32:43 -0500

    Take string or Buffer as data

--- lib/base64.ts
+++ lib/base64.ts
@@ -1,17 +1,26 @@
-export function encode(data: string = '') {
+export function encode(data: string | Buffer = ''): string {
+    if (data instanceof Buffer) {
+        return data.toString('base64');
+    }
+
     return new Buffer(data).toString('base64');
+
 }
 
-export function encodeSafe(data: string = '') {
+export function encodeSafe(data: string | Buffer = '') {
     data = encode(data);
     return data.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
 }
 
-export function decode (data: string = '') {
+export function decode (data: string | Buffer = '') {
+    if (data instanceof Buffer) {
+        return data.toString('utf8');
+    }
+
     return new Buffer(data, 'base64').toString('utf8');
 }
 
-export function decodeSafe(data: string = '') {
+export function decodeSafe(data: string | Buffer = '') {
     data = data.replace(/-/g, '+').replace(/_/g, '/');
 
     while (data.length % 4) {
